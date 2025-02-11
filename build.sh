927d588633d8aa01822c358c5321620b14d5bc89
Jeremy Graeme <Jeremy.Graeme@hey.com>
2025-02-10 21:30:06 -0800

    Add emacs to package installation and comment out system extensions setup

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs packages from fedora repos
-dnf install -y gnome-session-xsession tmux qtile qtile-extras rofi picom alacritty polybar xorg-x11-xinit xorg-x11-xinit-session libX11-devel libX11-xcb libXft-devel libXinerama-devel imlib2-devel arandr
+dnf install -y gnome-session-xsession tmux qtile qtile-extras rofi picom alacritty polybar xorg-x11-xinit xorg-x11-xinit-session libX11-devel libX11-xcb libXft-devel libXinerama-devel imlib2-devel arandr emacs
 
 # Use a COPR Example:
 #
@@ -26,38 +26,38 @@ dnf5 -y copr disable pgdev/ghostty
 #### Example for enabling a System Unit File
 
 # SYSEXTS?
-mkdir -p /etc/sysupdate.d/
-tee /usr/lib/tmpfiles.d/puma-sysext.conf <<EOF
-d /var/lib/extensions/ 0755 root root - -
-d /var/lib/extensions.d/ 0755 root root - -
-EOF
+# mkdir -p /etc/sysupdate.d/
+# tee /usr/lib/tmpfiles.d/puma-sysext.conf <<EOF
+# d /var/lib/extensions/ 0755 root root - -
+# d /var/lib/extensions.d/ 0755 root root - -
+# EOF
 
-SYSEXTS=(
-    emacs
-    google-chrome
-    keepassxc
-    microsoft-edge
-    vscode
-)
+# SYSEXTS=(
+#     emacs
+#     google-chrome
+#     keepassxc
+#     microsoft-edge
+#     vscode
+# )
 
-    for SYSEXT in "${SYSEXTS[@]}"; do
-        tee /etc/sysupdate.d/"$SYSEXT".conf <<EOF
-[Transfer]
-Verify=false
+#     for SYSEXT in "${SYSEXTS[@]}"; do
+#         tee /etc/sysupdate.d/"$SYSEXT".conf <<EOF
+# [Transfer]
+# Verify=false
 
-[Source]
-Type=url-file
-Path=https://github.com/travier/fedora-sysexts/releases/download/fedora-silverblue-41/
-MatchPattern=$SYSEXT-@v-%a.raw
+# [Source]
+# Type=url-file
+# Path=https://github.com/travier/fedora-sysexts/releases/download/fedora-silverblue-41/
+# MatchPattern=$SYSEXT-@v-%a.raw
 
-[Target]
-InstancesMax=2
-Type=regular-file
-Path=/var/lib/extensions.d/
-MatchPattern=$SYSEXT-@v-%a.raw
-CurrentSymlink=/var/lib/extensions/$SYSEXT.raw
-EOF
-    done
+# [Target]
+# InstancesMax=2
+# Type=regular-file
+# Path=/var/lib/extensions.d/
+# MatchPattern=$SYSEXT-@v-%a.raw
+# CurrentSymlink=/var/lib/extensions/$SYSEXT.raw
+# EOF
+#     done
 
 # leave this like bluefin-dx currently has it .
 # systemctl enable podman.socket
